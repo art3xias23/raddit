a5069f09daa3526c7f2f1cc5a9a4a233c0e05638
Konstantin Milchev <k.milchev.12@aberdeen.ac.uk>
2017-02-09 16:23:02 +0000

    bootstrap

--- db/migrate/20170209141129_add_user_id_to_links.rb
+++ db/migrate/20170209141129_add_user_id_to_links.rb
@@ -1,6 +1,6 @@
 class AddUserIdToLinks < ActiveRecord::Migration[5.0]
   def change
-    add_column :links, :user_id, :integer
-    add_index :links, :user_id
+    add_column :links, :user_id, :name, :integer
+    add_index :links, :user_id, :name
   end
 end
